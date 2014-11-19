7d6ec91a247f2085c0090c8d1a19ad65d74a0017
Keith Shaw <github@keithws.net>
2014-11-19 04:08:58 -0800

    Added HouseCode and UnitCode objects and refactored Address to use them.

--- test/test-x10.js
+++ test/test-x10.js
@@ -1,7 +1,138 @@
 var should = require("should"),
+	assert = require("assert"),
 	x10 = require("../x10"),
 	X10Address = x10.Address,
-	X10Command = x10.Command;
+	X10Command = x10.Command,
+	X10UnitCode = x10.UnitCode,
+	X10HouseCode = x10.HouseCode;
+
+describe("X10HouseCode", function () {
+	it("should create a house code object that is equal to a number", function () {
+		(function () { new X10HouseCode(""); }).should.throwError(/^Invalid house code.*/);
+		(function () { new X10HouseCode("AA"); }).should.throwError(/^Invalid house code.*/);
+		(function () { new X10HouseCode("1"); }).should.throwError(/^House code string out of range.*/);
+		(function () { new X10HouseCode("0"); }).should.throwError(/^House code string out of range.*/);
+		(function () { new X10HouseCode("Z"); }).should.throwError(/^House code string out of range.*/);
+		(function () { new X10HouseCode("Q"); }).should.throwError(/^House code string out of range.*/);
+
+		// test creating house code objects from hex values
+		assert.equal(new X10HouseCode(0x6), 0x6);
+		assert.equal(new X10HouseCode(0xE), 0xE);
+		assert.equal(new X10HouseCode(0x2), 0x2);
+		assert.equal(new X10HouseCode(0xA), 0xA);
+		assert.equal(new X10HouseCode(0x1), 0x1);
+		assert.equal(new X10HouseCode(0x9), 0x9);
+		assert.equal(new X10HouseCode(0x5), 0x5);
+		assert.equal(new X10HouseCode(0xD), 0xD);
+		assert.equal(new X10HouseCode(0x7), 0x7);
+		assert.equal(new X10HouseCode(0xF), 0xF);
+		assert.equal(new X10HouseCode(0x3), 0x3);
+		assert.equal(new X10HouseCode(0xB), 0xB);
+		assert.equal(new X10HouseCode(0x0), 0x0);
+		assert.equal(new X10HouseCode(0x8), 0x8);
+		assert.equal(new X10HouseCode(0x4), 0x4);
+		assert.equal(new X10HouseCode(0xC), 0xC);
+
+		// test creating house code objects from strings
+		assert.equal(new X10HouseCode("A"), 0x6);
+		assert.equal(new X10HouseCode("B"), 0xE);
+		assert.equal(new X10HouseCode("C"), 0x2);
+		assert.equal(new X10HouseCode("D"), 0xA);
+		assert.equal(new X10HouseCode("E"), 0x1);
+		assert.equal(new X10HouseCode("F"), 0x9);
+		assert.equal(new X10HouseCode("G"), 0x5);
+		assert.equal(new X10HouseCode("H"), 0xD);
+		assert.equal(new X10HouseCode("I"), 0x7);
+		assert.equal(new X10HouseCode("J"), 0xF);
+		assert.equal(new X10HouseCode("K"), 0x3);
+		assert.equal(new X10HouseCode("L"), 0xB);
+		assert.equal(new X10HouseCode("M"), 0x0);
+		assert.equal(new X10HouseCode("N"), 0x8);
+		assert.equal(new X10HouseCode("O"), 0x4);
+		assert.equal(new X10HouseCode("P"), 0xC);
+	});
+	it("should create house codes from numbers that are equal to ASCII characters", function () {
+		assert.equal(new X10HouseCode(0x6).toString(), "A");
+		assert.equal(new X10HouseCode(0xE).toString(), "B");
+		assert.equal(new X10HouseCode(0x2).toString(), "C");
+		assert.equal(new X10HouseCode(0xA).toString(), "D");
+		assert.equal(new X10HouseCode(0x1).toString(), "E");
+		assert.equal(new X10HouseCode(0x9).toString(), "F");
+		assert.equal(new X10HouseCode(0x5).toString(), "G");
+		assert.equal(new X10HouseCode(0xD).toString(), "H");
+		assert.equal(new X10HouseCode(0x7).toString(), "I");
+		assert.equal(new X10HouseCode(0xF).toString(), "J");
+		assert.equal(new X10HouseCode(0x3).toString(), "K");
+		assert.equal(new X10HouseCode(0xB).toString(), "L");
+		assert.equal(new X10HouseCode(0x0).toString(), "M");
+		assert.equal(new X10HouseCode(0x8).toString(), "N");
+		assert.equal(new X10HouseCode(0x4).toString(), "O");
+		assert.equal(new X10HouseCode(0xC).toString(), "P");
+	});
+});
+
+describe("X10UnitCode", function () {
+	it("should create a unit code object that is equal to a number", function () {
+		(function () { new X10UnitCode(""); }).should.throwError(/^Invalid unit code.*/);
+		(function () { new X10UnitCode("A"); }).should.throwError(/^Invalid unit code.*/);
+		(function () { new X10UnitCode("0"); }).should.throwError(/^Unit code string out of range.*/);
+		(function () { new X10UnitCode("17"); }).should.throwError(/^Unit code string out of range.*/);
+
+		// test creating house code objects from hex values
+		(new X10UnitCode(0x6)).valueOf().should.equal(0x6);
+		(new X10UnitCode(0xE)).valueOf().should.equal(0xE);
+		(new X10UnitCode(0x2)).valueOf().should.equal(0x2);
+		(new X10UnitCode(0xA)).valueOf().should.equal(0xA);
+		(new X10UnitCode(0x1)).valueOf().should.equal(0x1);
+		(new X10UnitCode(0x9)).valueOf().should.equal(0x9);
+		(new X10UnitCode(0x5)).valueOf().should.equal(0x5);
+		(new X10UnitCode(0xD)).valueOf().should.equal(0xD);
+		(new X10UnitCode(0x7)).valueOf().should.equal(0x7);
+		(new X10UnitCode(0xF)).valueOf().should.equal(0xF);
+		(new X10UnitCode(0x3)).valueOf().should.equal(0x3);
+		(new X10UnitCode(0xB)).valueOf().should.equal(0xB);
+		(new X10UnitCode(0x0)).valueOf().should.equal(0x0);
+		(new X10UnitCode(0x8)).valueOf().should.equal(0x8);
+		(new X10UnitCode(0x4)).valueOf().should.equal(0x4);
+		(new X10UnitCode(0xC)).valueOf().should.equal(0xC);
+
+		// test creating house code objects from strings
+		(new X10UnitCode("01")).valueOf().should.equal(0x6);
+		(new X10UnitCode("02")).valueOf().should.equal(0xE);
+		(new X10UnitCode("03")).valueOf().should.equal(0x2);
+		(new X10UnitCode("04")).valueOf().should.equal(0xA);
+		(new X10UnitCode("05")).valueOf().should.equal(0x1);
+		(new X10UnitCode("06")).valueOf().should.equal(0x9);
+		(new X10UnitCode("07")).valueOf().should.equal(0x5);
+		(new X10UnitCode("08")).valueOf().should.equal(0xD);
+		(new X10UnitCode("09")).valueOf().should.equal(0x7);
+		(new X10UnitCode("10")).valueOf().should.equal(0xF);
+		(new X10UnitCode("11")).valueOf().should.equal(0x3);
+		(new X10UnitCode("12")).valueOf().should.equal(0xB);
+		(new X10UnitCode("13")).valueOf().should.equal(0x0);
+		(new X10UnitCode("14")).valueOf().should.equal(0x8);
+		(new X10UnitCode("15")).valueOf().should.equal(0x4);
+		(new X10UnitCode("16")).valueOf().should.equal(0xC);
+	});
+	it("should create unit codes from numbers that are equal to ASCII characters", function () {
+		assert.equal(new X10UnitCode(0x6).toString(), "01");
+		assert.equal(new X10UnitCode(0xE).toString(), "02");
+		assert.equal(new X10UnitCode(0x2).toString(), "03");
+		assert.equal(new X10UnitCode(0xA).toString(), "04");
+		assert.equal(new X10UnitCode(0x1).toString(), "05");
+		assert.equal(new X10UnitCode(0x9).toString(), "06");
+		assert.equal(new X10UnitCode(0x5).toString(), "07");
+		assert.equal(new X10UnitCode(0xD).toString(), "08");
+		assert.equal(new X10UnitCode(0x7).toString(), "09");
+		assert.equal(new X10UnitCode(0xF).toString(), "10");
+		assert.equal(new X10UnitCode(0x3).toString(), "11");
+		assert.equal(new X10UnitCode(0xB).toString(), "12");
+		assert.equal(new X10UnitCode(0x0).toString(), "13");
+		assert.equal(new X10UnitCode(0x8).toString(), "14");
+		assert.equal(new X10UnitCode(0x4).toString(), "15");
+		assert.equal(new X10UnitCode(0xC).toString(), "16");
+	});
+});
 
 describe("X10Address", function () {
 	var address;
@@ -30,7 +161,7 @@ describe("X10Address", function () {
 	it("should create an address from a JSON string", function () {
 		address = new X10Address('{"houseCode":6,"unitCode":6}');
 		address.toString().should.equal("A1");
-		address.toJSON().should.equal('{"houseCode":6,"unitCode":6}');
+		address.toJSON().should.equal('{"houseCode":"A","unitCode":"01"}');
 	});
 
 	it("should create an address from an object", function () {
@@ -51,18 +182,22 @@ describe("X10Address", function () {
 
 	it("should have a proprty for the house code and unit code", function () {
 		address = new X10Address("A1");
-		address.houseCode.should.equal(0x6);
-		address.should.have.property("unitCode", 0x6);
+		houseCode = new X10HouseCode("A");
+		unitCode = new X10UnitCode("1");
+		should(address).have.properties({
+			"houseCode": houseCode,
+			"unitCode": unitCode
+		});
 	});
 
 	it("should throw an error when out of range", function () {
 		// House codes and device codes must be in the range from A to P and 1 to 16 respectively
-		(function () { new X10Address("A1"); }).should.not.throwError(/^Out of range.*/);
-		(function () { new X10Address("P16"); }).should.not.throwError(/^Out of range.*/);
-		(function () { new X10Address("Z1"); }).should.throwError(/^Out of range.*/);
-		(function () { new X10Address("Q1"); }).should.throwError(/^Out of range.*/);
-		(function () { new X10Address("A0"); }).should.throwError(/^Out of range.*/);
-		(function () { new X10Address("A17"); }).should.throwError(/^Out of range.*/);
+		(function () { new X10Address("A1"); }).should.not.throwError(/^Address out of range.*/);
+		(function () { new X10Address("P16"); }).should.not.throwError(/^Address out of range.*/);
+		(function () { new X10Address("Z1"); }).should.throwError(/^Address out of range.*/);
+		(function () { new X10Address("Q1"); }).should.throwError(/^Address out of range.*/);
+		(function () { new X10Address("A0"); }).should.throwError(/^Address out of range.*/);
+		(function () { new X10Address("A17"); }).should.throwError(/^Address out of range.*/);
 	});
 
 });
--- x10.js
+++ x10.js
@@ -8,86 +8,152 @@
 /**
  * Module Dependencies
  */
-var _ = require('underscore');
+var _ = require("underscore");
 
+// The housecodes and device codes range from A to P and 1 to 16 respectively although they do not follow a binary sequence. The encoding format for these codes is as follows:
 var deviceCodes = [0x6, 0xE, 0x2, 0xA, 0x1, 0x9, 0x5, 0xD, 0x7, 0xF, 0x3, 0xB, 0x0, 0x8, 0x4, 0xC];
 
+
 /**
- * object to convert X-10 address strings to their binary values
+ * object to convert X-10 house codes from numbers to strings and back
+ */
+function HouseCode(houseCode) {
+	this.raw = null;
+
+	switch (typeof houseCode) {
+	case "string":
+		if (houseCode !== "" && houseCode.length === 1) {
+			var index = houseCode.charCodeAt(0) - 65;
+			if ((index >= 0) && (index <= (deviceCodes.length - 1))) {
+				this.raw = deviceCodes[index];
+			} else {
+				throw new Error("House code string out of range.");
+			}
+		} else {
+			throw new Error("Invalid house code.");
+		}
+		break;
+	default:
+		if (deviceCodes.indexOf(houseCode) !== -1) {
+			this.raw = houseCode;
+		} else {
+			throw new Error("House code number out of range.");
+		}
+		break;
+	}
+	return this.raw;
+}
+
+HouseCode.prototype.toString = function(base) {
+	if (base !== undefined && base !== null) {
+		return this.raw.toString(base);
+	} else {
+		return String.fromCharCode(deviceCodes.indexOf(this.raw) + 65);
+	}
+};
+
+HouseCode.prototype.valueOf = function() {
+	return this.raw;
+};
+
+
+/**
+ * object to convert X-10 unit codes from numbers to strings and back
+ */
+function UnitCode(unitCode) {
+	switch (typeof unitCode) {
+	case "string":
+		unitCode = parseInt(unitCode, 10);
+		if (unitCode || unitCode === 0) {
+			var index = unitCode - 1;
+			if ((index >= 0) && (index <= (deviceCodes.length - 1))) {
+				this.raw = deviceCodes[index];
+			} else {
+				throw new Error("Unit code string out of range.");
+			}
+		} else {
+			throw new Error("Invalid unit code.");
+		}
+		break;
+	default:
+		if (deviceCodes.indexOf(unitCode) !== -1) {
+			this.raw = unitCode;
+		} else {
+			throw new Error("Unit code number out of range.");
+		}
+		break;
+	}
+	return this.raw;
+}
+
+UnitCode.prototype.toString = function(base) {
+	if (base !== undefined && base !== null) {
+		return this.raw.toString(base);
+	} else {
+		// always return two characters by padding with zero
+		return ("0" + (deviceCodes.indexOf(this.raw) + 1)).substr(-2, 2);
+	}
+};
+
+UnitCode.prototype.valueOf = function() {
+	return this.raw;
+};
+
+
+/**
+ * object to convert X-10 addresses from numbers to strings and back
  */
 function Address(address) {
-	// The housecodes and device codes range from A to P and 1 to 16 respectively although they do not follow a binary sequence. The encoding format for these codes is as follows:
 	this.houseCode = null;
 	this.unitCode = null;
-	switch(typeof address) {
-		case 'object':
-		if (_.isArray(address)) {
-			this.houseCode = address[0];
-			this.unitCode = address[1];
-		} else {
-			if (address.houseCode !== undefined && address.houseCode !== null) {
-				this.houseCode = address.houseCode;
+
+	try {
+		switch (typeof address) {
+		case "object":
+			if (_.isArray(address) && address.length === 2) {
+				this.houseCode = new HouseCode(address[0]);
+				this.unitCode = new UnitCode(address[1]);
+			} else {
+				if (address.houseCode !== undefined && address.houseCode !== null) {
+					this.houseCode = new HouseCode(address.houseCode);
+				}
+				if (address.unitCode !== undefined && address.unitCode !== null) {
+					this.unitCode = new UnitCode(address.unitCode);
+				}
 			}
-			if (address.unitCode !== undefined && address.unitCode !== null) {
-				this.unitCode = address.unitCode;
+			break;
+		case "string":
+			try {
+				address = JSON.parse(address);
+				if (address.hasOwnProperty("houseCode") && address.houseCode !== null) {
+					this.houseCode = new HouseCode(address.houseCode);
+				}
+				if (address.hasOwnProperty("unitCode") && address.unitCode !== null) {
+					this.unitCode = new UnitCode(address.unitCode);
+				}
+			} catch (e) {
+				this.houseCode = new HouseCode((address.toUpperCase().match(/[A-P]+/)||[null])[0]);
+				this.unitCode = new UnitCode((address.match(/\d+/)||[null])[0]);
 			}
+			break;
+		case "number":
+			this.houseCode = new HouseCode(address>>>4);
+			this.unitCode = new UnitCode(address&0x0F);
+			break;
+		default:
+			throw new Error("Invalid address.");
+			break;
 		}
-		break;
-		case 'number':
-		this.houseCode = address>>>4;
-		this.unitCode = address&0x0F;
-		break;
-		case 'string':
-		try {
-			address = JSON.parse(address);
-			if (address.hasOwnProperty("houseCode")) {
-				this.houseCode = address.houseCode;
-			}
-			if (address.hasOwnProperty("unitCode")) {
-				this.unitCode = address.unitCode;
-			}
-		} catch (e) {
-			this.houseCode = (address.toUpperCase().match(/[A-P]+/)||[null])[0];
-			this.unitCode = (address.match(/\d+/)||[null])[0];
-		}
-		break;
-	}
-	if (this.houseCode !== null) {
-		if (typeof this.houseCode === 'string') {
-			if (this.houseCode !== "") {
-				this.houseCode = this.houseCode.charCodeAt(0) - 65;
-				this.houseCode = deviceCodes[this.houseCode];
-			}
-		}
-	}
-	if (this.unitCode !== null) {
-		if (typeof this.unitCode === 'string') {
-			if (this.unitCode !== "") {
-				this.unitCode = parseInt(this.unitCode, 10) - 1;
-				this.unitCode = deviceCodes[this.unitCode];
-			}
-		}
-	}
-	if (!Number.isFinite(this.houseCode) || this.houseCode < 0x0 || this.houseCode > 0xf) {
-		// throw exception because code is out of allowed range
-		throw new Error("Out of range");
-	}
-	if (!Number.isFinite(this.unitCode) || this.unitCode < 0x0 && this.unitCode > 0xf) {
-		// throw exception because code is out of allowed range
-		throw new Error("Out of range");
+	} catch (e) {
+		throw new Error('Address out of range.');
 	}
 }
 
 Address.prototype.toString = function(base) {
-	var houseCode, unitCode;
 	if (base !== undefined && base !== null) {
 		return this.toNumber().toString(base);
 	} else {
-		houseCode = deviceCodes.indexOf(this.houseCode);
-		unitCode = deviceCodes.indexOf(this.unitCode);
-		houseCode = String.fromCharCode(houseCode + 65);
-		unitCode = unitCode + 1;
-		return houseCode + unitCode;
+		return this.houseCode.toString() + this.unitCode.toString().replace(/^0/, "");
 	}
 };
 
@@ -96,17 +162,22 @@ Address.prototype.toNumber = function() {
 };
 
 Address.prototype.toArray = function() {
-	return [ this.houseCode, this.unitCode ];
+	return [ this.houseCode.valueOf(), this.unitCode.valueOf() ];
 };
 
 Address.prototype.toObject = function() {
-	return { "houseCode": this.houseCode, "unitCode": this.unitCode };
+	return { "houseCode": this.houseCode.valueOf(), "unitCode": this.unitCode.valueOf() };
 };
 
-Address.prototype.toJSON = function() {
-	return JSON.stringify(this.toObject());
+Address.prototype.toJSON = function(base) {
+	return JSON.stringify(
+		{ "houseCode": this.houseCode.toString(base), "unitCode": this.unitCode.toString(base) }
+	);
 };
 
+Address.prototype.valueOf = function () {
+	return this.toNumber();
+};
 
 
 var functionCodes = [
@@ -230,7 +301,7 @@ function Command(command) {
 	this.functionCode = null;
 
 	switch (typeof command) {
-		case 'object':
+		case "object":
 			if (command instanceof Array) {
 				this.functionCode = command[0];
 			} else {
@@ -239,10 +310,10 @@ function Command(command) {
 				}
 			}
 		break;
-		case 'number':
+		case "number":
 		this.functionCode = command;
 		break;
-		case 'string':
+		case "string":
 		try {
 			command = JSON.parse(command);
 			if (command.hasOwnProperty("functionCode")) {
@@ -255,7 +326,7 @@ function Command(command) {
 	}
 
 	if (this.functionCode !== null) {
-		if (typeof this.functionCode === 'string') {
+		if (typeof this.functionCode === "string") {
 			if (this.functionCode !== "") {
 				this.functionCode = funcCodes.indexOf(this.functionCode);
 			}
@@ -268,6 +339,8 @@ Command.prototype.toObject = function () {
 };
 
 module.exports = {
+	HouseCode: HouseCode,
+	UnitCode: UnitCode,
 	Address: Address,
 	Command: Command
 };
